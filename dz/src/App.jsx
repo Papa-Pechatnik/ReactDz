5a5b4a8c614e9f1baf3a7c625452e8e43fbb699a
Yurij Sopun <yurakarpovichminsk@gmail.com>
2024-02-01 16:26:37 +0200

    #1.1

--- dz/src/App.jsx
+++ dz/src/App.jsx
@@ -3,7 +3,31 @@ import './App.css';
 const App = () => {
   return (
     <div className="content">
-    Start
+    <div className="box">
+     <img src='https://upload.wikimedia.org/wikipedia/uk/0/02/Homer_Simpson_2006.png' alt="Гомер Джей Сімпсон">
+      </img>
+      <h1>Гомер Джей Сімпсон</h1>
+    </div>
+    <div className="box">
+    <img src='https://upload.wikimedia.org/wikipedia/uk/0/0b/Marge_Simpson.png' alt="Марджері Сімпсон">
+      </img>
+      <h1>Марджері Сімпсон</h1>
+    </div>
+    <div className="box">
+      <img src='https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png' alt="Марджері Сімпсон">
+      </img>
+      <h1>Бартоломью Джо-Джо Сімпсон</h1>
+    </div>
+    <div className="box">
+      <img src='https://upload.wikimedia.org/wikipedia/uk/a/ae/Lisasmiling.png' alt="Елизабет Мері Сімпсон">
+      </img>
+      <h1>Елизабет Мері Сімпсон</h1>
+    </div>
+    <div className="box">
+      <img src='https://upload.wikimedia.org/wikipedia/uk/9/9d/Maggie_Simpson.png' alt="Маргарет Сімпсон">
+      </img>
+      <h1>Маргарет Сімпсон</h1>
+    </div>
     </div>
   );
 };
