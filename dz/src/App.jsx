f449e01319a9347b7b09aac85c2de7d0b4c38b1a
Yurij Sopun <yurakarpovichminsk@gmail.com>
2024-02-10 13:06:33 +0200

    errors

--- dz/src/App.jsx
+++ dz/src/App.jsx
@@ -51,7 +51,9 @@ const App = () => {
             ]
   return (
     <div>
-    {character.map(character=><Character character={character} key={character.id}/>)}
+    {/* {character.map(character => <Character character={character} key={character.id}/>)} */}
+    {character.map(character => <Character {...character} key={character.id}  />)}
+
 </div>
   );
 };
