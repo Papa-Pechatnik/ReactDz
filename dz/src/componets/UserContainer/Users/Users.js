8452af1583ab2bab18c4e21bdc4a3fa5ab539ce3
Yurij Sopun <yurakarpovichminsk@gmail.com>
2024-02-10 13:39:20 +0200

    start lesson2

---
+++ dz/src/componets/UserContainer/Users/Users.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Users = () => {
+    fetch ("https://jsonplaceholder.typicode.com/");
+    
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Users;
