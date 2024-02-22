4d48f4d7cbe7949b192288295e28805eee7aa758
Yurij Sopun <yurakarpovichminsk@gmail.com>
2024-02-22 07:21:49 +0200

    start useState(0)

--- dz/src/App.jsx
+++ dz/src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css';
+import { useState } from 'react';
 
 const App = () => {
+  const [count, setcount] = useState(0);
+  
   return (
     <div className="content">
-    Start
+    <div> count: { count }</div>
+    {/* варіант 1 - як колбек функція, сама себе повертає */}
+    {/* <button onClick={()=> setcount (count+1)}>кнопашка</button> */}
+
+    {/* варіант 2 - як колбек функція, сама себе повертає */}
+    <button onClick={()=> setcount (callBack => callBack + 1)}>кнопашка</button>
     </div>
   );
 };
